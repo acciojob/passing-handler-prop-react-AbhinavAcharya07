13bdd4f60c159bbdf015ff254459739098288396
Abhinav Acharya <abhinavacharya707@gmail.com>
2026-09-20 15:53:04 +0530

    Add Selection component with color application

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
+import React, { useState } from 'react';
+import '../styles/child.css';
 
+const Selection = ({ applyColor, subheading }) => {
+  const [style, setStyle] = useState({ background: '' });
+
+  return (
+    <div 
+      className='fix-box' 
+      style={style} 
+      onClick={() => applyColor(setStyle)}
+    >
+      <p className='subheading'>{subheading}</p>
+    </div>
+  );
+};
+
+export default Selection;
